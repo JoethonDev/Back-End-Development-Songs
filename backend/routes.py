d2a0506189eef55cea7cfa4aea4bd020dfba3a32
JoethonDev <elprincedoca@gmail.com>
2024-03-09 05:34:46 -0500

    implemented songs service

--- backend/routes.py
+++ backend/routes.py
@@ -62,5 +62,78 @@ def get_count():
 @app.route('/song')
 def songs():
     list_of_songs = db.songs.find({})
-    list_of_songs = json_util.dumps(list_of_songs)
+    list_of_songs = parse_json(list_of_songs)
     return {'songs' : list_of_songs}, 200
+
+@app.route('/song/<id>')
+def get_song_by_id(id):
+    try :
+        id = int(id)
+    except TypeError:
+        return {"message": "Invalid ID"}, 404
+
+    song = db.songs.find_one({"id": id})
+    if song :
+        song = parse_json(song)
+        return song, 200
+    else :
+        return {"message": "song with id not found"}, 404
+
+@app.route('/song', methods=['POST'])
+def create_song():
+    song = request.get_json()
+    try :
+        song['id'] = int(song['id'])
+    except TypeError :
+        return {'message' :  'Invalid ID'}, 302
+
+    exist_song = db.songs.find_one({'id' : song['id']})
+    if exist_song :
+        return {"Message" : f"song with id {song['id']} already present"}, 302
+    else :
+        record = db.songs.insert_one(song)
+        inserted_id = {
+            '$oid' : str(record.inserted_id)
+        }
+        return {"inserted id" : inserted_id}, 201
+
+@app.route('/song/<id>', methods=['PUT'])
+def update_song(id):
+    song = request.get_json()
+    try :
+        id = int(id)
+        query = {'id' : id}
+    except TypeError :
+        return {'message' :  'Invalid ID'}, 302
+
+    current_song = db.songs.find_one(query)
+    if not current_song :
+        return {"message": "song not found"}, 404
+    else :
+        current_song = parse_json(current_song)
+        is_changed = False
+        for key, val in song.items():
+            try :
+                if current_song[key] != val:
+                    is_changed = True
+                    break
+            except KeyError:
+                is_changed = True
+                break
+
+        if not is_changed:
+            return {"message":"song found, but nothing updated"}, 200
+
+        record = db.songs.update_one(query, {'$set': song})
+        updated_song = db.songs.find_one(query)
+        updated_song = parse_json(updated_song)
+
+        return updated_song, 200
+
+@app.route('/song/<int:id>', methods=['DELETE'])
+def delete_song(id):
+    deleted_song = db.songs.delete_one({'id' : id})
+    if deleted_song.deleted_count :
+        return "", 204
+    else :
+        return {"message": "song not found"}, 404
